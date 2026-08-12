046cf36417941f1bd907589f3c489fbcdd5ac26b
Arunav Pathak <139035104+pathakarunav123@users.noreply.github.com>
2026-08-13 00:16:20 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (81.36%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    int solve(int day,int stock, int fee,vector<vector<int>>&dp,vector<int>&prices){
-        if(day==prices.size())return 0;
-        if(dp[day][stock]!=-1) return dp[day][stock];
-        int maxi = -1e9;
-        if(stock==0){
-            int buy = -prices[day] + solve(day+1,1,fee,dp,prices);
-            int skip = solve(day+1,0,fee,dp,prices);
-            maxi = max(buy,skip);
-        }else{
-            int sell = prices[day] - fee + solve(day+1,0,fee,dp,prices);
-            int skip = solve(day+1,1,fee,dp,prices);
-            maxi = max(sell,skip);
-        }
-        return dp[day][stock]=maxi;
-    }
     int maxProfit(vector<int>& prices, int fee) {
-        int m = prices.size();
-        vector<vector<int>>dp(m+1,vector<int>(2,-1));
-        int ans = solve(0,0,fee,dp,prices);
-        return ans;
+        //alternate better approch
+        int cash = 0;
+        int hold = -prices[0];
+
+        for (int i = 1; i < prices.size(); i++) {
+            cash = max(cash, hold + prices[i] - fee);
+            hold = max(hold, cash - prices[i]);
+        }
+
+        return cash;
     }
 };
