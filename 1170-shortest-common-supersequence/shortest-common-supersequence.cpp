f32d6cdb9f0db57081a58d5c77b4f6a646bbc720
Arunav Pathak <139035104+pathakarunav123@users.noreply.github.com>
2026-08-18 19:18:14 +0530

    Time: 24 ms (64.65%) | Memory: 26.9 MB (18.13%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string str(int i, int j,vector<vector<int>>&dp,string &str1,string &str2, int scs_len){
+    string str(int i, int j,vector<vector<int>>&dp,string &str1,string &str2){
         string s;
         while(i<str1.size() && j<str2.size()){
 
@@ -10,9 +10,9 @@ public:
                 j++;
             }
              else{
-                int left = solve(i+1,j,dp,str1,str2);
-                int right = solve(i,j+1,dp,str1,str2);
-                if(left<right){
+           //     int left = solve(i+1,j,dp,str1,str2);
+            //    int right = solve(i,j+1,dp,str1,str2);
+                if(dp[i+1][j]<dp[i][j+1]){
                     s.push_back(str1[i]);
                     i++;
                 }else{
@@ -35,20 +35,36 @@ public:
         }
         return s;
     }
-    int solve(int i, int j,vector<vector<int>>&dp,string &str1, string &str2){
-        if(i==str1.size()) return str2.size()-j;
-        if(j==str2.size()) return str1.size()-i;
-        if(dp[i][j]!=-1) return dp[i][j];
+   // int solve(int i, int j,vector<vector<int>>&dp,string &str1, string &str2){
+    //    if(i==str1.size()) return str2.size()-j;
+    //   if(j==str2.size()) return str1.size()-i;
+    //    if(dp[i][j]!=-1) return dp[i][j];
 
-        if(str1[i]==str2[j]) return dp[i][j] = 1+solve(i+1,j+1,dp,str1,str2);
-        return dp[i][j] = min(1+solve(i+1,j,dp,str1,str2),1+solve(i,j+1,dp,str1,str2));
-    }
+    //    if(str1[i]==str2[j]) return dp[i][j] = 1+solve(i+1,j+1,dp,str1,str2);
+    //    return dp[i][j] = min(1+solve(i+1,j,dp,str1,str2),1+solve(i,j+1,dp,str1,str2));
+   // }
     string shortestCommonSupersequence(string str1, string str2) {
         int m = str1.size();
         int n = str2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        int scs_len = solve(0,0,dp,str1,str2);
-        string scs = str(0,0,dp,str1,str2,scs_len);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+     //   int scs_len = solve(0,0,dp,str1,str2);
+
+     for(int j=0; j<=n; j++){
+        dp[m][j] = n-j;
+     }
+     for(int i=0; i<=m; i++){
+        dp[i][n] = m-i;
+     }
+       for(int i=m-1; i>=0; i--){
+        for(int j=n-1; j>=0; j--){
+            if(str1[i]==str2[j]){
+                dp[i][j] = 1 + dp[i+1][j+1];
+            }else{
+                dp[i][j] = min(1+dp[i+1][j],1+dp[i][j+1]);
+            }
+        }
+       }
+        string scs = str(0,0,dp,str1,str2);
         return scs;
     }
 };
