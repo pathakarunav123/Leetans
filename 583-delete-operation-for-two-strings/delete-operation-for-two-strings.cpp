39a962334514ae4fb9b4b25bb858948bbf28ac3e
Arunav Pathak <139035104+pathakarunav123@users.noreply.github.com>
2026-08-21 14:27:31 +0530

    Time: 16 ms (21.85%) | Memory: 17 MB (21.43%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>&dp ,string &word1, string &word2){
-        if(i==word1.size() || j==word2.size()) return 0;
+    int solve(int i, int j,vector<vector<int>>&dp, string &word1, string &word2){
+        if(i==word1.size()){
+            return word2.size()-j;
+        }
+        if(j==word2.size()){
+            return word1.size()-i;
+        }
         if(dp[i][j]!=-1) return dp[i][j];
         if(word1[i]==word2[j]){
-            return dp[i][j] = 1 +solve(i+1,j+1,dp,word1,word2);
+            return dp[i][j] = solve(i+1,j+1,dp,word1,word2);
         }
-        return dp[i][j] = max(solve(i+1,j,dp,word1,word2),solve(i,j+1,dp,word1,word2));
+        return dp[i][j] = min(1+solve(i+1,j,dp,word1,word2),1+solve(i,j+1,dp,word1,word2));
     }
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
         int ans = solve(0,0,dp,word1,word2);
-        //total deletion
-        return (n+m)-2*ans;
+        return ans;
     }
 };
