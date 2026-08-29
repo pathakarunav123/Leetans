83454e6e64387c7f93e6905e1983bf35d95c6637
Arunav Pathak <139035104+pathakarunav123@users.noreply.github.com>
2026-08-29 23:37:27 +0530

    Time: 54 ms (5.01%) | Memory: 67.7 MB (5.01%) - LeetSync

---
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    const int MOD = 1e9+7;
+     const int OFFSET = 1000;
+    int solve(int i,int k,vector<vector<int>>&dp,int &arrLen){
+        if(k==0){
+            return (i==0);
+        }
+        if(i<0 || i>=arrLen) return 0;
+        if(dp[i+OFFSET][k]!=-1) return dp[i+OFFSET][k];
+        
+        int  left = solve(i-1,k-1,dp,arrLen);
+        
+        int right = solve(i+1,k-1,dp,arrLen);
+        int stay = solve(i,k-1,dp,arrLen);
+        int ans = ((long long)left+right+stay)%MOD;
+
+        return dp[i+OFFSET][k]=ans;
+
+    }
+
+    int numWays(int steps, int arrLen) {
+        vector<vector<int>>dp(3001,vector<int>(steps+1,-1));
+        int ans = solve(0,steps,dp,arrLen);
+        return ans;
+        
+    }
+};
