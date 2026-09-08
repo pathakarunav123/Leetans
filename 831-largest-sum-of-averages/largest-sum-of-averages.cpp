03220b75dca658a6b34091b3f88561a01877eea4
Arunav Pathak <139035104+pathakarunav123@users.noreply.github.com>
2026-09-08 22:36:14 +0530

    Time: 3 ms (82.91%) | Memory: 11.3 MB (31.16%) - LeetSync

---
+++ 831-largest-sum-of-averages/largest-sum-of-averages.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double solve(int i, vector<vector<double>>&dp, vector<int>&nums, int k){
+        if(k==0){
+            if(i==nums.size()){
+                return 0;
+            }
+            return -1e9;
+        }
+        if(dp[i][k]!=-1) return dp[i][k];
+        double ans = -1e9;
+        double sum =0;
+        for(int j=i; j<nums.size();j++){
+             sum+=nums[j];
+             ans = max(ans,sum/(j-i+1) + solve(j+1,dp,nums,k-1));
+        }
+        return dp[i][k]=ans;
+    }
+    double largestSumOfAverages(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<vector<double>>dp(n+1,vector<double>(n+1,-1));
+        double ans = solve(0,dp,nums,k);
+        return ans;
+    }
+};
