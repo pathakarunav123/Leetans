35849d37330b14379e1a1aa3c85cda9c65d09fce
Arunav Pathak <139035104+pathakarunav123@users.noreply.github.com>
2026-09-15 12:09:52 +0530

    Time: 363 ms (15.44%) | Memory: 253.9 MB (5.76%) - LeetSync

---
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+        bool isPal(int i, int j, vector<vector<int>>&dp1,string &s){
+            if(j==i+1) return (s[i]==s[j]);
+            if(i==j) return 1;
+            if(dp1[i][j]!=-1) return dp1[i][j];
+            if(s[i]==s[j]){
+                if(isPal(i+1,j-1,dp1,s)){
+                   return dp1[i][j]=1;
+                }
+            }
+            return dp1[i][j]=0;
+        }
+        int solve(int i, vector<int>&dp,vector<vector<int>>&dp1,string&s, int k){
+            if(i==s.size()) return 0;
+            if(dp[i]!=-1) return dp[i];
+            int take =0;
+            for(int j=i+k-1; j<s.size(); j++){
+                if(isPal(i,j,dp1,s)){
+                    take = max(take,1+solve(j+1,dp,dp1,s,k));
+                }
+            }
+           int not_take = solve(i+1,dp,dp1,s,k);
+           return dp[i] = max(take,not_take);
+        }
+    int maxPalindromes(string s, int k) {
+        int m = s.size();
+        vector<int>dp(m+1,-1);
+        vector<vector<int>>dp1(m+1,vector<int>(m+1,-1));
+        int ans = solve(0,dp,dp1,s,k);
+        return ans;
+    }
+};
